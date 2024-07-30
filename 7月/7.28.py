3954a38dcc9f437e5540a0a182030ff956f6baf3
Wjj20020718 <2053880471@qq.com>
2024-07-30 13:57:50 +0800

    7.28

--- 7月/7.28.py
+++ 7月/7.28.py
@@ -1,5 +1,8 @@
 # source:https://leetcode.cn/problems/find-the-maximum-number-of-marked-indices/ 二分+贪心 这道题第一次没贪好，写了两小时白费了。
 # 结论：从小到大排序后，如果存在 k 对匹配，那么一定可以让最小的 k 个数和最大的 k 个数匹配
+from typing import List
+
+
 class Solution:
     def maxNumOfMarkedIndices(self, nums: List[int]) -> int:
         nums.sort()
