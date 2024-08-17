64b594e274b9c4a88347f0abf6a75421b8b49124
Wjj20020718 <2053880471@qq.com>
2024-08-17 13:07:08 +0800

    8.17

---
+++ 8月/8.17.py
@@ -0,0 +1,31 @@
+# source:https://leetcode.cn/problems/minimum-number-of-operations-to-make-word-k-periodic/description/
+class Solution:
+    def minimumOperationsToMakeKPeriodic(self, word: str, k: int) -> int:
+        d = {}
+        for i in range(0, len(word)-k+1, k):
+            d[word[i:i+k]] = d.get(word[i:i+k], 0) + 1
+        res = 0
+        t = sorted(d.items(), key=lambda x:x[1])
+        for i in range(len(t)-1):
+            res += t[i][1]
+        return res
+
+# source:https://leetcode.cn/problems/sliding-subarray-beauty/description/
+from typing import List
+from sortedcontainers import SortedList
+class Solution:
+    def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
+        t = SortedList()
+        i = 0
+        res = []
+        for j, v in enumerate(nums):
+            t.add(v)
+            if j-i+1 > k:
+                t.remove(nums[i])
+                i += 1
+            if j-i+1 == k:
+                if t[x-1] < 0:
+                    res.append(t[x-1])
+                else:
+                    res.append(0)
+        return res
